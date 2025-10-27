99b4645945c06802c38e310c1fd4d5a24dbe41f7
Pedro <ppagbpedro@gmail.com>
2025-10-27 20:12:01 -0300

    chore: https setup for web app

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,5 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact()]
+  plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact()],
+  server: {
+    host: true,
+    https: {
+      cert: "./cert.pem",
+      key: "./key.pem"
+    }
+  }
 });
